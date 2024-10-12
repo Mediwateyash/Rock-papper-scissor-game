af8d21206e1c2f788c38c94e852f06c657e2c3ab
mediwateyash <diwateyash2004@gmail.com>
2024-10-12 23:14:31 +0530

    winner declare console

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ const genCompChoice = () => {
     return option[randIdx];
 };
 
+const drawGame = () => {
+    console.log("Game was draw.");
+    
+}
+
+const showWinner = (userwin) => {
+    if (userwin) {
+        console.log("You won the game.");
+    } else {
+        console.log("Computer won the game.");
+    } 
+    
+}
 // Function to play the game
 const playGame = (userChoice) => {
     console.log("user choice = ", userChoice);
@@ -17,7 +30,22 @@ const playGame = (userChoice) => {
     // Generate computer choice
     const compChoice = genCompChoice();
     console.log("computer choice = ", compChoice);
+
+    if ( userChoice === compChoice ) {
+        drawGame();
+    } else {
+        let userwin = true
+        if (userChoice === "rock") {
+            userwin = compChoice === "paper"? false: true
+        } else if (userChoice === "paper") {
+            userwin = compChoice === "scissor"? false: true
+        } else if (userChoice === "scissor") {
+            userwin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userwin);
+    }
 };
+    
 
 // Attach event listeners to each choice
 choices.forEach((choice) => {
